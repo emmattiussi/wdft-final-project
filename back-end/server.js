36339471df5c41d2b889f9611338089edf560dff
Emily Mattiussi <emily@axiom.local>
2017-03-13 19:03:59 -0400

    Parsed first RSS Feed

--- back-end/server.js
+++ back-end/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Run Server
 app.listen(8080, () => {
-    console.log('Server is running on http://localhost:8080. Press ctrl+C to stop.')
+    console.log('Server is running on http://localhost:8080. Press Ctrl+C to stop.')
 })
 
 // Get RSS Feeds
@@ -16,8 +16,13 @@ let cbc_indig;
 feed('http://www.cbc.ca/cmlink/rss-cbcaboriginal', (err, articles) => {
     if (err) throw err;
     cbc_indig = articles.map((element, index) => {
-        let $ = cheerio.load
-    })
+        let $ = cheerio.load(element.content);
+        let src = $('img').attr('src'); 
+        let content = $('p').text();
+        element["image"] = src; 
+        element["content"] = content; 
+        return element; 
+    }) 
 })
 
 
